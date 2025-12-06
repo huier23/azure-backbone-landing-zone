d9473e17da2cc4ca6bd9b0eda2dfcf1e8f41ecd7
Phil Huang <phil.huang@microsoft.com>
2025-12-06 23:22:40 +0800

    ✨ Refactor code structure for improved readability and maintainability
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- 4-terragrunt-local-dev-apply-specific.sh
+++ 4-terragrunt-local-dev-apply-specific.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+# XXX
+set -xeuo pipefail
 
 # 使用 Local State
-set -euo pipefail
-
 export TG_LOCAL_STATE=true
 export TF_VAR_use_msi=false
 export TF_VAR_use_cli=true
@@ -16,14 +16,13 @@ fi
 
 TARGET_DIR="$1"
 
-# 格式化 Terraform 配置文件
-terragrunt run -- fmt --recursive
+# 在目標目錄中執行 Terragrunt 指令
+(
+  cd "$TARGET_DIR"
 
-# 初始化 Terraform Modules
-terragrunt run --all init
+  # 使用 Terragrunt 執行特定目錄的 Terraform Plan
+  terragrunt plan
 
-# 使用 Terragrunt 執行特定目錄的 Terraform Plan
-terragrunt run-all plan --terragrunt-include-dir "$TARGET_DIR"
-
-# 使用 Terragrunt 執行特定目錄的 Terraform Apply
-terragrunt run-all apply --terragrunt-include-dir "$TARGET_DIR"
+  # 使用 Terragrunt 執行特定目錄的 Terraform Apply
+  terragrunt apply
+)
